d1c8a973f2f4db849fee99af463692a433c50057
Pablofasl7 <pablox2004x@gmail.com>
2024-04-18 13:37:20 +0200

    Campos añadidos y vista form mejorada

--- models/idea_management.py
+++ models/idea_management.py
@@ -7,8 +7,15 @@ class IdeaManagement(models.Model):
    _name = 'idea.management'
    _description = 'Idea'
 
-   name = fields.Char(string = 'Nombre de la idea', required=True)
+   name = fields.Char(string = 'Nombre de la idea')
    create_date = fields.Date(string = 'Fecha de creación', default=_date_default_today)
    deadline = fields.Date(string = 'Fecha límite')
-
-   employee_id = fields.Many2one(comodel_name='hr.employee', string='Empleado')
+   idea_type = fields.Selection(
+      [('mejoras', 'Mejoras'),
+      ('proyecto', 'Plantear proyecto'),], 
+      string="Tipo de idea")
+   details = fields.Text()
+   
+   company_id = fields.Many2one(comodel_name='res.partner', string='Compañía')
+   user_id = fields.Many2one(comodel_name='res.users', string='Empleados')
+   
